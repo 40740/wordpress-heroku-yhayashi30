d2a2973861b2402fb4d5d8b0506beb9daa34b5ff
yhayashi30 <yoshiyuki.hayashi@unisys.co.jp>
2019-05-01 19:37:29 +0900

    remove ja setting

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,9 +5,3 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
-
-/** Add Japanease */
-define ('WPLANG', 'ja');
-define ('WPLANG', 'admin-ja');
-define ('WPLANG', 'admin-network-ja');
-define ('WPLANG', 'continents-cities-ja');
